2ba3c3e719eaf6e4a10228c4b2ff282181a38873
Tim Wessels <wessels.t@hetnet.nl>
2018-06-28 10:42:45 +0200

    hoi

---
+++ GEZOND/Migrations/201806280842139_beter.cs
@@ -0,0 +1,16 @@
+namespace GEZOND.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class beter : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
